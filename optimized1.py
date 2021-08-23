a7c2269c781da9c44a250f863efa42b0f94944e2
maticha84 <maticha84@gmail.com>
2021-08-23 17:34:30 +0200

    first optimization : optimized1.py

--- optimized1.py
+++ optimized1.py
@@ -23,8 +23,35 @@ def recuperation_information(csv_import):
                     'profit': float(row['profit']),
                     'euros': round(float(row['price']) * float(row['profit']) / 100, 2)
                 }
-    dict_action = sorted(dict_action.items(), key=lambda x: x[1]['profit'], reverse=True)
+    dict_action = sorted(dict_action.items(), key=sort_by_profit)
     return dict_action
+
+
+def sort_by_profit(x):
+    """
+    sort by profit (in percent)
+    :param x:
+    :return: a sorted list of actions
+    """
+    return [-x[1]['profit'], -x[1]['price']]
+
+
+def sort_by_price(x):
+    """
+        sort by price (in euros)
+        :param x:
+        :return: a sorted list of actions
+        """
+    return [-x[1]['price'], -x[1]['profit']]
+
+
+def sort_by_euros(x):
+    """
+        sort by price (in euros)
+        :param x:
+        :return: a sorted list of actions
+        """
+    return [-x[1]['euros'], -x[1]['profit']]
 
 
 def calc_best_rendement(dict_action):
@@ -39,16 +66,42 @@ def calc_best_rendement(dict_action):
             list_actions.append(action[0])
             profit = profit + action[1]['euros']
 
-    result.append((list_actions, invest, profit))
+    result.append((list_actions, round(invest, 2), round(profit, 2)))
 
     return result
 
 
+def best_return(result):
+    profit = round(result[0][2] / result[0][1] * 100, 2)
+    return profit
+
+
 if __name__ == '__main__':
     time1 = time()
-    dict_actions = recuperation_information('data_20_actions.csv')
+    dict_actions_profit = recuperation_information('data_20_actions.csv')
+    dict_actions_price = sorted(dict_actions_profit, key=sort_by_price)
+    dict_actions_euros = sorted(dict_actions_profit, key=sort_by_euros)
+    result_profit = calc_best_rendement(dict_actions_profit)
+    result_price = calc_best_rendement(dict_actions_price)
+    result_euros = calc_best_rendement(dict_actions_euros)
 
-    result = calc_best_rendement(dict_actions)
-    print(result)
+    list_return = []
+    return_profit = best_return(result_profit)
+    return_price = best_return(result_price)
+    return_euros = best_return(result_euros)
+
+    list_return.append(return_profit)
+    list_return.append(return_price)
+    list_return.append(return_euros)
+
+    best_solution = max(list_return)
+    print(best_solution)
+    if best_solution == return_profit:
+        print(result_profit)
+    elif best_solution == return_price:
+        print(result_price)
+    else:
+        print(result_euros)
+
     time2 = time() - time1
     print(f'\nExecution : {time2}s')
