305c6da83a7dbf53adbb1f019205a420b1194954
maticha84 <maticha84@gmail.com>
2021-08-24 11:39:37 +0200

    first optimization : optimized1.py This optimisation is derived from the so-called "rucksack optimisation". The best returns are taken into account and added to the maximum sum of 500€.

--- optimized1.py
+++ optimized1.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 # coding: utf-8
-
+"""
+This optimisation is derived from the so-called "rucksack optimisation".
+The best returns are taken into account and added to the maximum sum of 500€.
+"""
 import csv
 from time import time
 
@@ -36,24 +39,6 @@ def sort_by_profit(x):
     return [-x[1]['profit'], -x[1]['price']]
 
 
-def sort_by_price(x):
-    """
-        sort by price (in euros)
-        :param x:
-        :return: a sorted list of actions
-        """
-    return [-x[1]['price'], -x[1]['profit']]
-
-
-def sort_by_euros(x):
-    """
-        sort by price (in euros)
-        :param x:
-        :return: a sorted list of actions
-        """
-    return [-x[1]['euros'], -x[1]['profit']]
-
-
 def calc_best_rendement(dict_action):
     invest = 0
     profit = 0
@@ -79,29 +64,9 @@ def best_return(result):
 if __name__ == '__main__':
     time1 = time()
     dict_actions_profit = recuperation_information('data_20_actions.csv')
-    dict_actions_price = sorted(dict_actions_profit, key=sort_by_price)
-    dict_actions_euros = sorted(dict_actions_profit, key=sort_by_euros)
     result_profit = calc_best_rendement(dict_actions_profit)
-    result_price = calc_best_rendement(dict_actions_price)
-    result_euros = calc_best_rendement(dict_actions_euros)
 
-    list_return = []
-    return_profit = best_return(result_profit)
-    return_price = best_return(result_price)
-    return_euros = best_return(result_euros)
-
-    list_return.append(return_profit)
-    list_return.append(return_price)
-    list_return.append(return_euros)
-
-    best_solution = max(list_return)
-    print(best_solution)
-    if best_solution == return_profit:
-        print(result_profit)
-    elif best_solution == return_price:
-        print(result_price)
-    else:
-        print(result_euros)
+    print(f'profit {result_profit}')
 
     time2 = time() - time1
     print(f'\nExecution : {time2}s')
